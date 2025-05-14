c9692bab8677b4710614b8d506874332b9bfc370
simranjit02 <simranjitsingh58648@gmail.com>
2025-05-14 16:00:42 +0300

    initial commit

---
+++ controller/App.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+  "use strict";
+
+  return Controller.extend("my.app.controller.App", {
+    onInit: function () {
+      // Initialization code
+    },
+  });
+});
