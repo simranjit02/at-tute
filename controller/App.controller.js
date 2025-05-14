15facf31123d6e952f2b376c1bdec47e7c737dd5
simranjit02 <simranjitsingh58648@gmail.com>
2025-05-14 16:15:36 +0300

    fix folder structure

--- controller/App.controller.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-  "use strict";
-
-  return Controller.extend("my.app.controller.App", {
-    onInit: function () {
-      // Initialization code
-    },
-  });
-});
